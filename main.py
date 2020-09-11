cd2cd394cbbace544e5cfbe073c1aa002a2f9967
methylDragon <methylDragon@gmail.com>
2020-09-12 01:31:41 +0800

    Use rmf_adapter's navgraph parsing function instead

--- main.py
+++ main.py
@@ -53,66 +53,6 @@ def sanitise_dict(dictionary, inplace=False, recursive=False):
     return output
 
 
-def create_nav_graph(config):
-    """Construct RMF navigation graph from config file."""
-    nav_graph = graph.Graph()
-    waypoint_index_offset = 0  # Offset to account for prior maps
-
-    for map_name, map_config in config.get('rmf_maps', {}).items():
-        # Add waypoints
-        for waypoint in map_config.get('waypoints', []):
-            nav_graph.add_waypoint(map_name, waypoint)
-
-        # Set holding points
-        for holding_index in map_config.get('holding_points', []):
-            waypoint = nav_graph.get_waypoint(waypoint_index_offset
-                                              + holding_index)
-            waypoint.set_holding_point(True)
-
-        # Set passthrough points
-        for passthrough_index in map_config.get('passthrough_points', []):
-            waypoint = nav_graph.get_waypoint(waypoint_index_offset
-                                              + passthrough_index)
-            waypoint.set_passthrough_point(True)
-
-        # Set parking spots
-        for parking_index in map_config.get('parking_spots', []):
-            waypoint = nav_graph.get_waypoint(waypoint_index_offset
-                                              + parking_index)
-            waypoint.set_parking_spot(True)
-
-        # Add lanes
-        for lane in map_config.get('lanes', []):
-            lane = [graph.lane.Node(waypoint_index_offset + x)
-                    for x in lane]
-            nav_graph.add_lane(*lane)
-
-        # Add bidirectional lanes
-        for bidir_lane in map_config.get('bidir_lanes', []):
-            bidir_lane = [waypoint_index_offset + x for x in bidir_lane]
-            nav_graph.add_bidir_lane(*bidir_lane)
-
-        # Add dock lanes
-        for dock_name, dock in map_config.get('dock_lanes', {}).items():
-            if not dock.get('lane'):
-                continue
-
-            dock_lane = [waypoint_index_offset + x for x in dock['lane']]
-            dock_seconds = dock.get('seconds', 10)
-
-            nav_graph.add_dock_lane(*dock_lane,
-                                    str(dock_name),
-                                    dock_seconds)
-
-        # Add keys
-        for key_name, waypoint_index in map_config.get('keys', {}).items():
-            nav_graph.add_key(str(key_name),
-                              waypoint_index_offset + waypoint_index)
-
-        waypoint_index_offset = nav_graph.num_waypoints
-    return nav_graph
-
-
 def compute_transforms(rmf_coordinates, mir_coordinates, node=None):
     """Get transforms between RMF and MIR coordinates."""
     transforms = {
@@ -129,8 +69,8 @@ def compute_transforms(rmf_coordinates, mir_coordinates, node=None):
     return transforms
 
 
-def create_fleet(config, nav_graph, mock):
-    """Create RMF Adapter and FleetUpdateHandle."""
+def create_fleet(config, mock):
+    """Create RMF Adapter, FleetUpdateHandle, and parse navgraph."""
     profile = traits.Profile(
         geometry.make_final_convex_circle(
             config['rmf_fleet']['profile']['radius']
@@ -141,6 +81,8 @@ def create_fleet(config, nav_graph, mock):
         angular=traits.Limits(*config['rmf_fleet']['limits']['angular']),
         profile=profile
     )
+
+    nav_graph = graph.parse_graph(config['map_path'], robot_traits)
 
     # RMF_CORE Fleet Adapter: Manages delivery or loop requests
     if mock:
@@ -160,7 +102,7 @@ def create_fleet(config, nav_graph, mock):
     else:
         fleet.accept_delivery_requests(delivery_condition)
 
-    return adapter, fleet, fleet_name, profile
+    return adapter, fleet, fleet_name, profile, nav_graph
 
 
 def create_robot_command_handles(config, handle_data, dry_run=False):
@@ -282,13 +224,9 @@ def main(args, delivery_condition=None, mock=False):
     rclpy.init()
     adpt.init_rclcpp()
 
-    # INIT GRAPH ==============================================================
-    nav_graph = create_nav_graph(config)
-
     # INIT FLEET ==============================================================
-    adapter, fleet, fleet_name, profile = create_fleet(config,
-                                                       nav_graph,
-                                                       mock=mock)
+    adapter, fleet, fleet_name, profile, nav_graph = create_fleet(config,
+                                                                  mock=mock)
 
     # INIT TRANSFORMS =========================================================
     rmf_coordinates = config['reference_coordinates']['rmf']
